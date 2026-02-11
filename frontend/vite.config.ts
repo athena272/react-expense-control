93a748d58175c7c5a564ece5ca10310fca8e4ff0
rosario <58920070+athena272@users.noreply.github.com>
2026-02-11 19:02:48 -0300

    chore(frontend): scaffolding do projeto e configuracoes

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/components/shared', import.meta.url)),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+})
